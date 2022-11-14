f6184577db8c6d6fc9265b0c2ba576eebfeb0e6a
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-13 18:49:21 -0800

    Adding pages

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Cart.modules.scss"
+
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
---
+++ src/Pages/OrderHIstory/OrderHistory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./OrderHistory.modules.scss"
+
+function OrderHistory() {
+  return (
+    <div>OrderHistory</div>
+  )
+}
+
+export default OrderHistory
