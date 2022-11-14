31f1cf92c925e6e802105c378a6c15192dc0ab0c
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-13 19:48:30 -0800

    Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+// Components
+import { Header , Footer} from './Components';
+// Pages
+import { Home, Contact } from "./Pages"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        
+        <Route path="/" element={< Home /> } />
+        <Route path="/contact" element={< Contact /> } />
+        {/* <Route path="/" element={< Home /> } >
+        <Route path="/" element={< Home /> } >
+        <Route path="/" element={< Home /> } > */}
+
+      </Routes>
+    <Footer />
+    </BrowserRouter>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Footer.module.scss"
+
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
