dc6a9f9407ac44f3efd92bc0654147eece2e6beb
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-15 09:48:24 -0800

    Verify password and confirm password by notification using ReactToastify

--- src/Pages/auth/Register.js
+++ src/Pages/auth/Register.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.scss"
 import registerImg from "../../assets/eshop-register.png"
 import { Link } from 'react-router-dom'
 import Card from '../../Components/card/Card'
 import { FaGoogle } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Register = () => {
-  return (
-    <section className={`container ${styles.auth}`}>
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
-    <Card>
-        <div className={styles.form}>
-            <h2>Register</h2>
-            <form>
-                <input type="text" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
-                <input type="password" placeholder="Confirm Password" required />
-                <button className="--btn --btn-danger --btn-block">Register</button>
-            </form>
-            <button className="--btn --btn-primary --btn-block"><FaGoogle color='#fff' />Register With Google</button>
-            <p>Already have an account? <Link to="/login">Login</Link></p>                    
-        </div>
-    </Card>
+    const registerUser = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            toast.error("Password and confirm password not match");
+        }
+        console.log(email, password, confirmPassword);
+    };
 
-    <div className={styles.img}>
-        <img src={registerImg} alt="Register" width="500" />
-    </div>
-</section>
-  )
+    return (
+        <>
+            <ToastContainer />
+            <section className={`container ${styles.auth}`}>
+                <Card>
+                    <div className={styles.form}>
+                        <h2>Register</h2>
+                        <form onSubmit={registerUser}>
+                            <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input type="password" placeholder="Confirm Password" required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                            <button type='submit' className="--btn --btn-danger --btn-block">Register</button>
+                        </form>
+                        <button className="--btn --btn-primary --btn-block"><FaGoogle color='#fff' />Register With Google</button>
+                        <p>Already have an account? <Link to="/login">Login</Link></p>
+                    </div>
+                </Card>
+
+                <div className={styles.img}>
+                    <img src={registerImg} alt="Register" width="500" />
+                </div>
+            </section>
+        </>
+    )
 }
 export default Register
