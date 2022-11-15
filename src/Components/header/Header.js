1569202c76d6bf46b449508d462e6db756a01546
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-15 15:04:51 -0800

    Show diferente header menu is logged in or log out

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -9,6 +9,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch } from 'react-redux'
 import { SET_ACTIVE_USER, REMOVE_ACTIVE_USER } from '../../redux/slice/authSlice'
+import ShowOnLogin from "../../Components/hiddenLink/HiddenLink"
+import ShowOnLogOut from "../../Components/hiddenLink/HiddenLink"
 
 
 const logo = (
@@ -28,7 +30,7 @@ const carts = (
 )
 
 
-const activeLink = ({ isActive }) => (isActive? `${styles.active}` : "")
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "")
 
 const Header = () => {
   const navigate = useNavigate()
@@ -57,21 +59,21 @@ const Header = () => {
   // Monitor currently sign in user
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
-        if(user.displayName == null){
+        if (user.displayName == null) {
           // const u1 = user.email.slice(0, -10); // to remove only the @gmail.com
           const u1 = user.email.substring(0, user.email.indexOf("@"));
           const uName = u1.charAt(0).toUpperCase + u1.slice(1);
           setDisplayUserName(uName)
-        }else{
+        } else {
           setDisplayUserName(user.displayName);
-        }        
+        }
         dispatch(SET_ACTIVE_USER({
           email: user.email,
-          userName: user.displayName? user.displayName : displayUserName,
+          userName: user.displayName ? user.displayName : displayUserName,
           userID: user.uid,
         }))
 
@@ -87,24 +89,32 @@ const Header = () => {
       <div className={styles.header}>
         {logo}
         <nav className={showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
-          <div className={showMenu? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}` : `${styles["nav-wrapper"]}`} onClick={hideMenu}>
+          <div className={showMenu ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}` : `${styles["nav-wrapper"]}`} onClick={hideMenu}>
           </div>
 
-            <ul onClick={hideMenu}>
-              <li className={styles["logo-mobile"]}>{logo} <FaTimes size={22} color="#fff" onClick={hideMenu}/></li>
-              <li><NavLink to="/" className={activeLink}>Home</NavLink></li>
-              <li><NavLink to="/contact" className={activeLink}>Contact Us</NavLink></li>
-            </ul>
-            <div className={styles["header-right"]} onClick={hideMenu}>
-              <span className={styles.links}>
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>{logo} <FaTimes size={22} color="#fff" onClick={hideMenu} /></li>
+            <li><NavLink to="/" className={activeLink}>Home</NavLink></li>
+            <li><NavLink to="/contact" className={activeLink}>Contact Us</NavLink></li>
+          </ul>
+          <div className={styles["header-right"]} onClick={hideMenu}>
+            <span className={styles.links}>
+
+              <ShowOnLogOut>
                 <NavLink to="/login" className={activeLink}>Login</NavLink>
-                <a  href="#"><FaUserCircle size={18}/> Hi, {displayUserName}</a>
-                <NavLink to="/register" className={activeLink}>Register</NavLink>
-                <NavLink to="/order-history" className={activeLink}>My Orders </NavLink>
+                <a href="#home"><FaUserCircle size={18} /> Hi, {displayUserName}</a>
+              </ShowOnLogOut>
+
+              <NavLink to="/register" className={activeLink}>Register</NavLink>
+              <NavLink to="/order-history" className={activeLink}>My Orders </NavLink>
+
+              <ShowOnLogin>
                 <NavLink to="/" className={activeLink} onClick={logoutUser}>Log Out </NavLink>
-              </span>
-              {carts}
-            </div>
+              </ShowOnLogin>
+
+            </span>
+            {carts}
+          </div>
         </nav>
 
         <div className={styles["menu-icon"]}>
