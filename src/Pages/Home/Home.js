397c37d8f7e5a143a270a2b5fe2d5c787394f9b5
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-15 19:46:10 -0800

    eshop Home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from "../../Components/slider/Slider"
+import Product from "../../Components/product/Product";
 import styles from "./Home.module.scss"
 
 
-function Home() {
+const Home = () => {
+
+  const url = window.location.href;
+
+  useEffect(() => {
+    const scrollToProducts = () => {
+      if (url.includes("#products")) {
+        window.scrollTo({
+          top: 700,
+          behavior: "smooth",
+        });
+        return;
+      }
+    };
+    scrollToProducts();
+  }, [url]);
   return (
     <div>
       <Slider />
+      <Product />
     </div>
   )
 }
