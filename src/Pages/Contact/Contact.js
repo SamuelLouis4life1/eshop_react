f67e299322b7a1bc6051c2d563c78bd0f9c2449b
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-15 19:20:24 -0800

    package and contact form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,14 +1,99 @@
 import React from 'react'
+import { useRef } from "react";
+import Card from "../../Components/card/Card";
 import styles from "./Contact.module.scss"
+import { FaPhoneAlt, FaEnvelope, FaTwitter } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
+const Contact = () => {
+  const form = useRef();
 
-function Contact() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(form.current);
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        "template_7xyhwen",
+        form.current,
+        "user_hKs2aRfLoozcqA28UpUyz"
+      )
+      .then(
+        (result) => {
+          toast.success("Message sent successfully");
+        },
+        (error) => {
+          toast.error(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-      
-    </div>
-  )
-}
+    <section>
+      <div className={`container ${styles.contact}`}>
+        <h2>Contact Us</h2>
+        <div className={styles.section}>
+          <form ref={form} onSubmit={sendEmail}>
+            <Card cardClass={styles.card}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full Name"
+                required
+              />
+              <label>Email</label>
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Your active email"
+                required
+              />
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+              <label>Message</label>
+              <textarea name="message" cols="30" rows="10"></textarea>
+              <button className="--btn --btn-primary">Send Message</button>
+            </Card>
+          </form>
 
-export default Contact
+          <div className={styles.details}>
+            <Card cardClass={styles.card2}>
+              <h3>Our Contact Information</h3>
+              <p>Fill the form or contact us via other channels listed below</p>
+              <div className={styles.icons}>
+                <span>
+                  <FaPhoneAlt />
+                  <p>+55 (21)98772-2151</p>
+                </span>
+                <span>
+                  <FaEnvelope />
+                  <p>Sampeur4life1@gmail.com</p>
+                </span>
+                <span>
+                  <GoLocation />
+                  <p>Rio de Janeiro, Brazil</p>
+                </span>
+                <span>
+                  <FaTwitter />
+                  <p>@samuel.louis.sampeur</p>
+                </span>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
