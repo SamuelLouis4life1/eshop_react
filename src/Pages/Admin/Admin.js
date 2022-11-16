be12cf3238677f40949244d0acab366e02c35a59
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2022-11-15 19:40:21 -0800

    Admin dashboard

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import styles from "./Admin.module.scss"
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "../../Components/admin/addProduct/AddProduct";
+import Home from "../../Components/admin/home/Home";
+import Navbar from "../../Components/admin/navbar/Navbar";
+import OrderDetails from "../../Components/admin/orderDetails/OrderDetails";
+import Orders from "../../Components/admin/orders/Orders";
+import ViewProducts from "../../Components/admin/viewProducts/ViewProducts";
 
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className={styles.admin}>
+      <div className={styles.navbar}>
+        <Navbar />
+      </div>
+      <div className={styles.content}>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="all-products" element={<ViewProducts />} />
+          <Route path="add-product/:id" element={<AddProduct />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="order-details/:id" element={<OrderDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default Admin
